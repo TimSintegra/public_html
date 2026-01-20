2cd0514333cb037cd458d50bbed54416dc37d9bf
TimSintegra <shabalintimon@ya.ru>
2026-01-20 23:49:10 +0300

    Убрал надпись из сертификатов

--- templates/default/modules/certificates/show.tpl
+++ templates/default/modules/certificates/show.tpl
@@ -2,6 +2,5 @@
     <div class="container">
         <h2 class="section-title">{$header_result}</h2>
         <h3 class="subsection-title">{$header_certificates}</h3>
-        <p>Список сертификатов (добавьте изображения или ссылки).</p> 
     </div>
 </section>
--- templates_c/59943a4f3cc8efb3bfa9dac983d732be87457c5e_0.file_show.tpl.php
+++ templates_c/59943a4f3cc8efb3bfa9dac983d732be87457c5e_0.file_show.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 5.4.3, created on 2025-12-11 16:42:52
+/* Smarty version 5.4.3, created on 2026-01-20 21:48:12
   from 'file:/opt/lampp/htdocs/public_html/templates/default/modules/certificates/show.tpl' */
 
 /* @var \Smarty\Template $_smarty_tpl */
 if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   'version' => '5.4.3',
-  'unifunc' => 'content_693ae67cb902c6_64970874',
+  'unifunc' => 'content_696fea0c747565_69959528',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '59943a4f3cc8efb3bfa9dac983d732be87457c5e' => 
     array (
       0 => '/opt/lampp/htdocs/public_html/templates/default/modules/certificates/show.tpl',
-      1 => 1765467470,
+      1 => 1768942088,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   array (
   ),
 ))) {
-function content_693ae67cb902c6_64970874 (\Smarty\Template $_smarty_tpl) {
+function content_696fea0c747565_69959528 (\Smarty\Template $_smarty_tpl) {
 $_smarty_current_dir = '/opt/lampp/htdocs/public_html/templates/default/modules/certificates';
 ?><section class="certificates-section">
     <div class="container">
@@ -28,7 +28,6 @@ $_smarty_current_dir = '/opt/lampp/htdocs/public_html/templates/default/modules/
 </h2>
         <h3 class="subsection-title"><?php echo $_smarty_tpl->getValue('header_certificates');?>
 </h3>
-        <p>Список сертификатов (добавьте изображения или ссылки).</p> 
     </div>
 </section><?php }
 }
